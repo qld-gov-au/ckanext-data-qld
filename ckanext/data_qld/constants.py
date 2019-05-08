32277209bae996d84564555adaea6a678a7eaf88
Mark Calvert <mark.calvert@salsadigital.com.au>
2019-05-08 17:26:14 -0600

    Developed new overrides of datarequest extension Added new email templates for data requests Refactored code

---
+++ ckanext/data_qld/constants.py
@@ -0,0 +1,7 @@
+DATAREQUESTS_MAIN_PATH = 'datarequest'
+SHOW_DATAREQUEST = 'show_datarequest'
+UPDATE_DATAREQUEST = 'update_datarequest'
+UPDATE_DATAREQUEST_ORGANISATION = 'update_datarequest_organisation'
+CLOSE_DATAREQUEST = 'close_datarequest'
+OPEN_DATAREQUEST = 'open_datarequest'
+LIST_DATAREQUESTS = 'list_datarequests'
