f0dd9da80718e51f11cbf264363d332d1bdc55c5
Nathan Perry <nathan@salsadigital.com.au>
2019-05-15 17:50:37 +1200

    [DQL-46] Add GTM snippet to Data.Qld theme.

--- ckanext/data_qld_theme/plugin.py
+++ ckanext/data_qld_theme/plugin.py
@@ -1,9 +1,18 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckan.common import config
+
+
+def get_gtm_code():
+    # To get Google Tag Manager Code
+    gtm_code = config.get('ckan.google_tag_manager.gtm_container_id', False)
+    return str(gtm_code)
+
 
 class DataQldThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +20,9 @@ class DataQldThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'data_qld_theme')
+
+    # ITemplateHelpers
+    def get_helpers(self):
+        return {
+            'get_gtm_container_id': get_gtm_code
+        }
