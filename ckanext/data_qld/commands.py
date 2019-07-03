afbecddc657da3e9491c048828daee6c405161ea
MarkCalvert <37602611+MarkCalvert@users.noreply.github.com>
2019-07-02 23:43:23 -0600

    Training to develop (#28)
    * Added validation extension resource Schema meta data
    
    * Added google analytics from ckan extension google analytics and added customisations for data qld.
    Added configurable dictionary of api's to capture and send configurable event label to google analytics
    
    * Adding config settings to readme
    
    * Implementing changes to open resource data schema
    
    * Moved the capture_api_actions config from .ini to it's on JSON file
    Refactored code to use capture_api_actions and google_analytics_id on loading config settings
    Refactored google analytics code to its own folder
    
    * Updated subjects of data request emails
    
    * Fixed resource size not being set by uploaded filesize
    
    * Updated readme
    
    * Removed main_css and site_custom_css sections from config screen
    
    * Update README.md
    
    * Update README.md
    
    * Moved data request suggested description into trans block
    
    * Change the resource size field to be non-mandatory (#13)
    
    * Feature/dql 25 comments enhancements (#12)
    
    * [DQL-25] Helper functions for comments.
    
    * [DQL-25] Comments
    - Remove non-Data.Qld specific comment helper functions
    - Add helper function to display comment count on Data Request comment tab
    
    * [DQL-25] Remove profanity import from ckanext-data-qld helpers.
    
    * Update README.md
    
    * Fixed "parameter" spelling mistake
    
    * Applying code formatting changes.
    
    * Feature/dql 51 data request not assigned to your organisation anymore (#18)
    
    * Email Admin users of unassigned organisation
    
    * Fix code formatting reported by linter
    
    * Send comment emails via a background job for better optimisation (#20)
    
    * [DQL-91] Redirect users to user/reset after password reset request. (#21)
    
    * Added org admin's access to open and close a datarequest from its own… (#19)
    
    * Added org admin's access to open and close a datarequest from its own organisation
    
    * Code tidy-up from lint report
    
    * Code tidy-up from lint report
    
    * Update README.md
    
    * Removed next review date from resource meta data schema (#24)
    
    * Removed next review date from resource meta data schema
    Added update frequency to dataset meta data schema
    Update migration script to set default value for update frequency and removed expiration date
    
    * [DQL-21] Code tidy up and add details to README.md
    
    * [DQL-88] Changes for POC group configuration. (#23)
    
    * Updated readme, removing unnecessary step in migrating legacy extra fields
    
    * Added note about paster migration command
    
    * Feature/dql 101 demo feedback   28.06.2019 (#27)
    
    * Updated license and url fields to be required in the metadata scheme
    
    * Added new resource url upload preset to make the field mandatory
    
    * [DQL-10] Add paster command for demoting publishers to editor capacity. (#26)
    
    * [DQL-10] Add paster command for demoting publishers to editor capacity.
    
    * [DQL-104] Updated README.

--- ckanext/data_qld/commands.py
+++ ckanext/data_qld/commands.py
@@ -124,3 +124,54 @@ class MigrateExtras(CkanCommand):
             self.update_package(package_id, security_classification, data_driven_application, version, author_email, notes, update_frequency, resources)
 
         return 'SUCCESS'
+
+
+class DemotePublishers(CkanCommand):
+    """Demotes any existing 'publisher-*' users from admin to editor in their respective organisations
+    """
+
+    summary = __doc__.split('\n')[0]
+
+    def __init__(self, name):
+
+        super(DemotePublishers, self).__init__(name)
+        self.parser.add_option('-u', '--username_prefix', dest='username_prefix', help='Only demote usernames starting with this prefix', type=str, default='publisher-')
+
+    def get_organizations(self):
+        return toolkit.get_action('organization_list')(data_dict={'all_fields': True, 'include_users': True})
+
+    def patch_organisation_users(self, org_id, users):
+        toolkit.get_action('organization_patch')(data_dict={'id': org_id, 'users': users})
+
+    def command(self):
+        """
+
+        :return:
+        """
+        self._load_config()
+
+        username_prefix = self.options.username_prefix
+
+        updates = 0
+
+        for org in self.get_organizations():
+            print('- - - - - - - - - - - - - - - - - - - - - - - - -')
+            updates_required = False
+            users = org.get('users', [])
+            print('Processing organisation ID: %s | Name: %s' % (org['id'], org['name']))
+            if users:
+                for user in org['users']:
+                    if user['name'].startswith(username_prefix) and user['capacity'] == 'admin':
+                        print('- Setting capacity for user %s to "editor" in organisation %s' % (user['name'], org['name']))
+                        user['capacity'] = 'editor'
+                        updates_required = True
+                        updates += 1
+                if updates_required:
+                    print('- Updating user capacities for organisation %s' % org['name'])
+                    self.patch_organisation_users(org['id'], users)
+                else:
+                    print('- Nothing to update for organisation %s' % org['name'])
+
+        print('- - - - - - - - - - - - - - - - - - - - - - - - -')
+
+        return "COMPLETED. Total updates %s\n" % updates
