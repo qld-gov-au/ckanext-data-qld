bf425b6315d9da1d880fc4f560d9dd85fdabd9b4
william dutton <will.dutt@gmail.com>
2020-07-30 14:17:36 +1000

    flake8

--- ckanext/data_qld/reporting/helpers/helpers.py
+++ ckanext/data_qld/reporting/helpers/helpers.py
@@ -45,7 +45,7 @@ def get_report_date_range(request):
     if end_date:
         end_date = toolkit.h.date_str_to_datetime(end_date)
     else:
-        #interal ckan datetimes is utc, presentation is ckan_timezone
+        # interal ckan datetimes is utc, presentation is ckan_timezone
         end_date = datetime.utcnow()
 
     return start_date.date().isoformat(), end_date.date().isoformat()
