ff6bd9ac708e34ce3106bfaaa276a350abbfac8c
william dutton <will.dutt@gmail.com>
2020-07-30 13:38:00 +1000

    all internal datetimes in ckan are utc

--- ckanext/data_qld/actions.py
+++ ckanext/data_qld/actions.py
@@ -1,5 +1,3 @@
-import pytz
-
 import ckan.lib.base as base
 import ckan.lib.helpers as helpers
 import ckan.lib.mailer as mailer
@@ -176,7 +174,7 @@ def create_datarequest(original_action, context, data_dict):
     data_req = db.DataRequest()
     _undictize_datarequest_basic(data_req, data_dict)
     data_req.user_id = context['auth_user_obj'].id
-    data_req.open_time = datetime.datetime.now(tzinfo=pytz.utc)
+    data_req.open_time = datetime.datetime.utcnow()
 
     session.add(data_req)
     session.commit()
@@ -337,7 +335,7 @@ def close_datarequest(original_action, context, data_dict):
 
     data_req.closed = True
     data_req.accepted_dataset_id = data_dict.get('accepted_dataset_id') or None
-    data_req.close_time = datetime.datetime.now(tzinfo=pytz.utc)
+    data_req.close_time = datetime.datetime.utcnow()
     _undictize_datarequest_closing_circumstances(data_req, data_dict)
 
     session.add(data_req)
