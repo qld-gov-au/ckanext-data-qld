c95515bc21abdd5d56dc559bd215c56f7082fb0d
MarkCalvert <37602611+MarkCalvert@users.noreply.github.com>
2021-09-15 21:31:39 -0500

    Feature/DQL3-4-currency-email-notification (#40)
    * [DQL3-4]
    - Updated 'validate_nature_of_change_data' to check for the 'nature_of_change' value even if data has not been updated and remove any values for new resources
    - Updated 'resource_visibility' to check if value exists in list of options
    - Fixed issue with request endpoint not existing, updated to use params which will exist on flask or pylon requests

--- ckanext/data_qld/resource_freshness/validation.py
+++ ckanext/data_qld/resource_freshness/validation.py
@@ -5,8 +5,8 @@ import datetime as dt
 
 from ckanext.data_qld.resource_freshness.helpers import helpers as h
 
-missing = df.missing
-StopOnError = df.StopOnError
+missing = tk.missing
+StopOnError = tk.StopOnError
 
 
 def validate_next_update_due(keys, flattened_data, errors, context):
@@ -46,16 +46,21 @@ def validate_nature_of_change_data(keys, flattened_data, errors, context):
 
     if resource.get('id'):
         # Resource updated
-        # # The resource_data_updated value is set in  the 'before_update' IResource interface method 'check_resource_data'
+        # Only validate the current resource being updated
+        # The resource_data_updated value is set in  the 'before_update' IResource interface method 'check_resource_data'
         resource_data_updated = context.get('resource_data_updated', {})
-        if resource_data_updated.get('id') == resource.get('id') and \
-           resource_data_updated.get('data_updated', False) is True:
-            # Resource data has updated so the nature_of_change validation is required
-            if not nature_of_change or nature_of_change is missing:
-                errors[keys].append(tk._('Missing value'))
-            elif nature_of_change == 'add-new-time-series' and update_frequency in h.get_update_frequencies():
-                # Resource data has updated and the correct nature of change provided
+        if resource_data_updated and resource_data_updated.get('id') == resource.get('id'):
+            if resource_data_updated.get('data_updated', False) is True:
+                # Resource data has updated so the nature_of_change validation is required
+                if not nature_of_change or nature_of_change is missing:
+                    errors[keys].append(tk._('Missing value'))
+                    raise StopOnError
+
+            # If nature_of_change is selected check value to see if the correct nature of change provided
+            if nature_of_change == 'add-new-time-series' and update_frequency in h.get_update_frequencies():
                 h.resource_data_updated(flattened_data, update_frequency, next_update_due, index, errors, context)
     else:
         # Resource created
         h.resource_data_updated(flattened_data, update_frequency, next_update_due, index, errors, context)
+        # Should not have a nature_of_change so remove it
+        flattened_data.pop(keys, None)
--- ckanext/data_qld/resource_visibility/validators.py
+++ ckanext/data_qld/resource_visibility/validators.py
@@ -1,8 +1,8 @@
-import ckan.lib.navl.dictization_functions as df
 import ckan.plugins.toolkit as toolkit
 
-missing = df.missing
+missing = toolkit.missing
 h = toolkit.h
+StopOnError = toolkit.StopOnError
 
 
 def resource_visibility(key, data, errors, context):
@@ -10,7 +10,6 @@ def resource_visibility(key, data, errors, context):
     Users must select one of the correct option
     based on the package.de_identified_data.
     """
-    options = h.data_qld_get_select_field_options('resource_visibility')
     de_identified_data = data.get((u'de_identified_data',))
 
     res, index, field = key
@@ -18,25 +17,31 @@ def resource_visibility(key, data, errors, context):
     resource_data_updated = context.get('resource_data_updated', {})
 
     # None is a new resource.
-    processed_res_id = None
-    if resource_data_updated:
-        processed_res_id = resource_data_updated.get('id')
+    processed_res_id = resource_data_updated.get('id') if resource_data_updated else None
+    value = str(data.get(key, '')) if data[key] is not missing else ''
 
-    if data[key] is not missing:
-        value = str(data.get(key, ''))
+    if res_id == processed_res_id:
+        options = h.data_qld_get_select_field_options('resource_visibility')
+        option_values = [option.get('value') for option in options]
+        if de_identified_data == 'YES':
+            # Field is required.
+            if len(value) == 0:
+                errors[key].append(toolkit._(
+                    'This dataset has been recorded as containing de-identified data. A re-identification risk governance acknowledgement for this resource must be made prior to publishing'))
+                raise StopOnError
+            # First option will be disabled.
+            if value == options[0].get('value'):
+                errors[key].append(toolkit._('Illegal option selected'))
+                raise StopOnError
+        else:
+            # Second option will be disabled.
+            if value == options[1].get('value'):
+                errors[key].append(toolkit._('Illegal option selected'))
+                raise StopOnError
+            # Blank empty strings are allowed for non de_identified_data
+            option_values.append('')
 
-        if res_id == processed_res_id:
-            if de_identified_data == 'YES':
-                # Field is required.
-                if len(value) == 0:
-                    errors[key].append(toolkit._('This dataset has been recorded as containing de-identified data. A re-identification risk governance acknowledgement for this resource must be made prior to publishing'))
-
-                # First option will be disabled.
-                if value == options[0].get('value'):
-                    errors[key].append(toolkit._('Illegal option selected'))
-            else:
-                # Second option will be disabled.
-                if value == options[1].get('value'):
-                    errors[key].append(toolkit._('Illegal option selected'))
-    else:
-        data.pop(key, None)
+        # Check if value is part of the options
+        if value not in option_values:
+            errors[key].append(toolkit._('Value must be one of {}'.format(option_values)))
+            raise StopOnError
