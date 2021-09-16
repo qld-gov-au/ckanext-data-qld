d02f270d6f326c9a66b06ddc0b7174cc9b4eed4c
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-09-16 14:21:30 +1000

    [QOL-8117] cleanup
    - sync login testing with other repos
    - add GitHub test badge
    - ignore deprecated Flake8 rule

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -11,15 +11,28 @@ def go_to_home(context):
 
 @step('I log in')
 def log_in(context):
-
     assert context.persona
     context.execute_steps(u"""
         When I go to homepage
         And I click the link with text that contains "Log in"
-        And I fill in "login" with "$name"
+        And I log in directly
+    """)
+
+
+@step('I log in directly')
+def log_in_directly(context):
+    """
+    This differs to the `log_in` function above by logging in directly to a page where the user login form is presented
+    :param context:
+    :return:
+    """
+
+    assert context.persona
+    context.execute_steps(u"""
+        When I fill in "login" with "$name"
         And I fill in "password" with "$password"
         And I press the element with xpath "//button[contains(string(), 'Login')]"
-        Then I should see an element with xpath "//a[contains(string(), 'Log out')]"
+        Then I should see an element with xpath "//a[@title='Log out']"
     """)
 
 
