765b17c1f050ffbdf168be82713f49fcfe35b9ff
Mark Calvert <markcalvert84@gmail.com>
2021-09-18 12:59:41 -0600

    Fixing merge issues

--- ckanext/data_qld/resource_freshness/validation.py
+++ ckanext/data_qld/resource_freshness/validation.py
@@ -95,25 +95,3 @@ def data_last_updated(key, flattened_data, errors, context):
     # Should not have a nature_of_change so remove it
     flattened_data.pop(keys, None)
 
-
-def data_last_updated(key, flattened_data, errors, context):
-    '''
-    Validate last data updated
-    '''
-    key, = key
-    data = unflatten(flattened_data)
-    # Get package with 'pakcage_sho' because the validator doesnt have the all data required
-    package = get_action('package_show')(context, data)
-    resources = package.get('resources')
-    last_updated = get_validator('isodate')(package.get('data_last_updated', ""), context)
-    # Cycle through the resources to compare data_last_updated field with last_modified
-    for resource in resources:
-        last_modified = get_validator('isodate')(resource.get('last_modified', ""), context)
-        if last_modified is None:
-            return
-        if last_updated is None:
-            last_updated = last_modified
-        if last_modified > last_updated:
-            last_updated = last_modified
-
-    flattened_data[('data_last_updated', )] = get_validator('convert_to_json_if_datetime')(last_updated, context)
