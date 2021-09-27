004a8250d32cc8a7cbdf29649e8084b8bff66ae9
Mark Calvert <markcalvert84@gmail.com>
2021-09-27 11:54:39 -0600

    Updated user validators to throw invalid exception to stop validation

--- ckanext/data_qld/user_creation/validators.py
+++ ckanext/data_qld/user_creation/validators.py
@@ -8,7 +8,7 @@ def data_qld_user_name_validator(key, data, errors, context):
     is_sysadmin = user is not None and user.sysadmin
 
     if not is_sysadmin and 'publisher' in data[key].lower():
-        errors[key].append('The username cannot contain the word \'publisher\'. Please enter another username.')
+        raise toolkit.Invalid('The username cannot contain the word \'publisher\'. Please enter another username.')
 
 
 def data_qld_displayed_name_validator(key, data, errors, context):
@@ -19,4 +19,5 @@ def data_qld_displayed_name_validator(key, data, errors, context):
         excluded_names = config.get('ckanext.data_qld.excluded_display_name_words', '').split('\r\n')
         for name in excluded_names:
             if name.lower() in data[key].lower():
-                errors[key].append('The displayed name cannot contain certain words such as \'publisher\', \'QLD Government\' or similar. Please enter another display name.')
+                raise toolkit.Invalid(
+                    'The displayed name cannot contain certain words such as \'publisher\', \'QLD Government\' or similar. Please enter another display name.')
