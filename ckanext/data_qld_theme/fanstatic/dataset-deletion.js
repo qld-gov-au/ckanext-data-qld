86899235bb22e1f0a270f8cba8d890700531f08f
Mark Calvert <markcalvert84@gmail.com>
2021-10-13 08:34:07 -0600

    Added check for '?' character in href url. Conditional add '?' to the end if it does not exist else add '&'

--- ckanext/data_qld_theme/fanstatic/dataset-deletion.js
+++ ckanext/data_qld_theme/fanstatic/dataset-deletion.js
@@ -1,13 +1,14 @@
 jQuery(document).ready(function () {
     var dataset_delete_button = jQuery('#dataset-delete-button')
     original_href = dataset_delete_button.attr('href')
+    original_href = (original_href.indexOf('?') == -1) ? original_href.concat('?') : original_href.concat('&')
     dataset_delete_button.removeAttr('disabled')
-    
+
     jQuery('body').on('keyup', '#deletion-reason', function (event) {
         var value = jQuery(event.target).val().trim();
         var btn_primary = jQuery('#modal-deletion-reason').find('.modal-footer .btn-primary')
         if (value.length >= 10) {
-            new_href = original_href.concat('&deletion_reason=', value)
+            new_href = original_href.concat('deletion_reason=', value)
             dataset_delete_button.attr('href', new_href);
             btn_primary.removeAttr('disabled');
         } else {
