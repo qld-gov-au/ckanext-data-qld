8b3e79483b7be8d9aeea6d8d9dbc8ffd9bec92e5
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-10-15 11:32:16 +1000

    [QOL-8117] fix case for required fields

--- .docker/scripts/create-test-data.sh
+++ .docker/scripts/create-test-data.sh
@@ -164,7 +164,7 @@ echo "Creating test dataset for reporting:"
 curl -LsH "Authorization: ${API_KEY}" \
     --data "name=reporting&description=Dataset for reporting&owner_org=${REPORT_ORG_ID}&\
 update_frequency=near-realtime&author_email=report_admin@localhost&version=1.0&license_id=cc-by-4\
-&data_driven_application=no&security_classification=PUBLIC&notes=test&de_identified_data=no"\
+&data_driven_application=NO&security_classification=PUBLIC&notes=test&de_identified_data=NO"\
     ${CKAN_ACTION_URL}/package_create
 
 echo "Creating test Data Request for reporting:"
