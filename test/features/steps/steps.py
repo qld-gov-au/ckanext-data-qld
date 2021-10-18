717f36e8500041574d72f3d1f27905b337a4b305
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-10-18 13:43:47 +1000

    [QOL-8117] fix base64 padding under Python 2
    Python 3 seems to handle this better

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -208,8 +208,11 @@ def should_receive_base64_email_containing_text(context, address, text):
         payload = mail.get_payload()
         print("[DEBUG] Raw email payload: {}".format(payload))
         payload += "=" * ((4 - len(payload) % 4) % 4)  # do fix the padding error issue
-        print("[DEBUG] Raw email payload after padding fix: {}".format(payload))
-        decoded_payload = quopri.decodestring(payload).decode('base64')
+        payload_bytes = quopri.decodestring(payload)
+        if len(payload_bytes) > 0:
+            payload_bytes += b'='  # do fix the padding error issue
+        print("[DEBUG] Raw email payload after padding fix: {}".format(payload_bytes))
+        decoded_payload = payload_bytes.decode('base64')
         print('decoded_payload: ', decoded_payload)
         return text in decoded_payload
 
