b76813b9cdb08ff86529ca71662ee35b4fab6b18
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-10-19 10:37:29 +1000

    [QOL-8117] drop extension setup that might have broken tests

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -12,8 +12,6 @@ pip install -r "requirements-dev.txt"
 pip install -r "$VENV_DIR/src/ckanext-archiver/requirements.txt"
 pip install -r "$VENV_DIR/src/ckanext-data-qld-theme/requirements.txt"
 pip install -r "$VENV_DIR/src/ckanext-dcat/requirements.txt"
-pip install -r "$VENV_DIR/src/ckanext-scheming/requirements.txt"
-pip install -r "$VENV_DIR/src/ckanext-validation/requirements.txt"
 pip install -r "$VENV_DIR/src/ckanext-ytp-comments/requirements.txt"
 python setup.py develop
 installed_name=$(grep '^\s*name=' setup.py |sed "s|[^']*'\([-a-zA-Z0-9]*\)'.*|\1|")
