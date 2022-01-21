bb2f59f8df76dc908f1118033efe3e6a6bf1ea30
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-01-21 14:37:36 +1000

    [QOL-7811] sync test config with Publications

--- .docker/scripts/init.sh
+++ .docker/scripts/init.sh
@@ -4,16 +4,12 @@
 #
 set -e
 
-CKAN_USER_NAME="${CKAN_USER_NAME:-admin}"
-CKAN_DISPLAY_NAME="${CKAN_DISPLAY_NAME:-Administrator}"
-CKAN_USER_PASSWORD="${CKAN_USER_PASSWORD:-Password123!}"
-CKAN_USER_EMAIL="${CKAN_USER_EMAIL:-admin@localhost}"
-
 if [ "$VENV_DIR" != "" ]; then
   . ${VENV_DIR}/bin/activate
 fi
-ckan_cli db clean
+CLICK_ARGS="--yes" ckan_cli db clean
 ckan_cli db init
+ckan_cli db upgrade
 
 # Initialise validation tables
 PASTER_PLUGIN=ckanext-validation ckan_cli validation init-db
@@ -31,12 +27,6 @@ PASTER_PLUGIN=ckanext-report ckan_cli report initdb
 
 # Initialise the QA database tables
 PASTER_PLUGIN=ckanext-qa ckan_cli qa init
-
-ckan_cli user add "${CKAN_USER_NAME}"\
- fullname="${CKAN_DISPLAY_NAME}"\
- email="${CKAN_USER_EMAIL}"\
- password="${CKAN_USER_PASSWORD}"
-ckan_cli sysadmin add "${CKAN_USER_NAME}"
 
 # Create some base test data
 . $APP_DIR/scripts/create-test-data.sh
