42b3eb1f4e4f8f29469652a5a9b5fa6d88dfb2a2
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-02-02 14:34:53 +1000

    [QOL-8595] clean up testing steps

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -2,7 +2,6 @@ from behave import step
 from behaving.personas.steps import *  # noqa: F401, F403
 from behaving.web.steps import *  # noqa: F401, F403
 from behaving.web.steps.url import when_i_visit_url
-from behaving.mail.steps import *
 import email
 import quopri
 import random
@@ -13,12 +12,17 @@ def get_current_url(context):
     context.browser.evaluate_script("document.documentElement.clientWidth")
 
 
-@step('I go to homepage')
+@step(u'I go to homepage')
 def go_to_home(context):
     when_i_visit_url(context, '/')
 
 
-@step('I log in')
+@step(u'I go to register page')
+def go_to_register_page(context):
+    when_i_visit_url(context, '/user/register')
+
+
+@step(u'I log in')
 def log_in(context):
     assert context.persona
     context.execute_steps(u"""
@@ -28,7 +32,7 @@ def log_in(context):
     """)
 
 
-@step('I log in directly')
+@step(u'I log in directly')
 def log_in_directly(context):
     """
     This differs to the `log_in` function above by logging in directly to a page where the user login form is presented
@@ -54,17 +58,27 @@ def title_random_text(context):
     """.format(random.randrange(1000)))
 
 
-@step('I go to organisation page')
+@step(u'I go to dataset page')
+def go_to_dataset_page(context):
+    when_i_visit_url(context, '/dataset')
+
+
+@step(u'I go to dataset "{name}"')
+def go_to_dataset(context, name):
+    when_i_visit_url(context, '/dataset/' + name)
+
+
+@step(u'I edit the "{name}" dataset')
+def edit_dataset(context, name):
+    when_i_visit_url(context, '/dataset/edit/{}'.format(name))
+
+
+@step(u'I go to organisation page')
 def go_to_organisation_page(context):
     when_i_visit_url(context, '/organization')
 
 
-@step('I go to register page')
-def go_to_register_page(context):
-    when_i_visit_url(context, '/user/register')
-
-
-@step('I log in and go to the data requests page')
+@step(u'I log in and go to the data requests page')
 def log_in_go_to_datarequest_page(context):
     assert context.persona
     context.execute_steps(u"""
@@ -73,12 +87,12 @@ def log_in_go_to_datarequest_page(context):
     """)
 
 
-@step('I go to the data requests page')
+@step(u'I go to the data requests page')
 def go_to_datarequest_page(context):
     when_i_visit_url(context, '/datarequest')
 
 
-@step('I log in and create a datarequest')
+@step(u'I log in and create a datarequest')
 def log_in_create_a_datarequest(context):
 
     assert context.persona
@@ -88,7 +102,7 @@ def log_in_create_a_datarequest(context):
     """)
 
 
-@step('I create a datarequest')
+@step(u'I create a datarequest')
 def create_datarequest(context):
 
     assert context.persona
@@ -101,19 +115,9 @@ def create_datarequest(context):
     """)
 
 
-@step('I go to my reports page')
+@step(u'I go to my reports page')
 def go_to_reporting_page(context):
     when_i_visit_url(context, '/dashboard/reporting')
-
-
-@step('I go to dataset page')
-def go_to_dataset_page(context):
-    when_i_visit_url(context, '/dataset')
-
-
-@step(u'I go to dataset "{name}"')
-def go_to_dataset(context, name):
-    when_i_visit_url(context, '/dataset/' + name)
 
 
 @step(u'I go to dataset "{name}" comments')
