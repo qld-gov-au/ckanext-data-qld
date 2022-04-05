68f1d355ca5954a18b61b1ce68295bf7376a3edc
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-04-05 16:36:24 +1000

    [QOL-7939] provide default API version via parameter default instead of Flask default
    - this is more targeted to the function where it's needed, and more reliable

--- ckanext/data_qld/google_analytics/blueprints.py
+++ ckanext/data_qld/google_analytics/blueprints.py
@@ -2,25 +2,24 @@
 
 import flask
 
-from ckan.views.api import _get_request_data, action as core_action
+from ckan.views.api import _get_request_data, action as core_action, API_DEFAULT_VERSION
 from ckantoolkit import get_action
 
 from controller_functions import record_api_action
 
 
-def action(api_action, ver=None):
+def action(api_action, ver=API_DEFAULT_VERSION):
     function = get_action(api_action)
     side_effect_free = getattr(function, 'side_effect_free', False)
     request_data = _get_request_data(try_url_params=side_effect_free)
     record_api_action(api_action, request_data)
-    return core_action(api_action, ver)
+    return core_action(api_action, ver=ver)
 
 
 blueprint = flask.Blueprint(
     u'data_qld_google_analytics',
     __name__,
-    url_prefix='/api',
-    url_defaults={'ver': 3}
+    url_prefix='/api'
 )
 
 blueprint.add_url_rule(u'/action/<api_action>', view_func=action, methods=('GET', 'POST'))
