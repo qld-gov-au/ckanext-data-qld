b71145b59c25a2f2d1c6650f0ccd079054442806
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-04-21 15:26:55 +1000

    [QOL-8779] add Flask config to expose header snippet

--- ckanext/data_qld/blueprints.py
+++ ckanext/data_qld/blueprints.py
@@ -2,8 +2,14 @@
 
 from flask import Blueprint
 
+from ckantoolkit import render
+
 from constants import DATAREQUESTS_MAIN_PATH, SCHEMA_MAIN_PATH
 from controller_functions import open_datarequest, show_schema
+
+
+def header_snippet():
+    return render('header.html')
 
 
 blueprint = Blueprint(
@@ -17,3 +23,4 @@ blueprint.add_url_rule(
 blueprint.add_url_rule(
     u'/dataset/<dataset_id>resource/<resource_id>/{}/show'.format(SCHEMA_MAIN_PATH),
     view_func=show_schema)
+blueprint.add_url_rule(u'/header.html', view_func=header_snippet)
