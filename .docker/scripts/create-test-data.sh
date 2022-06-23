738efe9646a2ef8d7dff1667f9983904d3775973
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-06-23 12:30:05 +1000

    [DC-31965] use a valid email address for test author email

--- .docker/scripts/create-test-data.sh
+++ .docker/scripts/create-test-data.sh
@@ -106,7 +106,7 @@ curl -LsH "Authorization: ${API_KEY}" \
 # Create public test dataset with our standard fields
 curl -LsH "Authorization: ${API_KEY}" \
     --data '{"name": "public-test-dataset", "owner_org": "'"${TEST_ORG_ID}"'",
-"update_frequency": "monthly", "author_email": "admin@localhost", "version": "1.0",
+"update_frequency": "monthly", "author_email": "admin@example.com", "version": "1.0",
 "license_id": "other-open", "data_driven_application": "NO", "security_classification": "PUBLIC",
 "notes": "test", "de_identified_data": "NO"}' \
     ${CKAN_ACTION_URL}/package_create
