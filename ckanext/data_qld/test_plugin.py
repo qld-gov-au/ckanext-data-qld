cf9fcfbfa577fbf1598935b58d74b0d773b9b91c
mutantsan <kvaqich@gmail.com>
2022-08-19 15:01:29 +0300

    add test_plugin for bdd tests

---
+++ ckanext/data_qld/test_plugin.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+import ckantoolkit as tk
+
+from ckan import plugins
+
+from ckanext.data_qld.tests.conftest import DatasetFactory
+
+
+class DataQldTestPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IActions)
+
+    # IActions
+
+    def get_actions(self):
+        return {
+            'qld_test_create_dataset': qld_test_create_dataset,
+            'qld_test_purge_dataset': qld_test_purge_dataset
+        }
+
+
+@tk.side_effect_free
+def qld_test_create_dataset(context, data_dict):
+    tk.check_access("site_read", context)
+    return DatasetFactory(**data_dict)
+
+@tk.side_effect_free
+def qld_test_purge_dataset(context, data_dict):
+    context = {"ignore_auth": True}
+    user_obj = tk.get_action("get_site_user")(context, {})
+    return tk.get_action("dataset_purge")({"user": user_obj['name']}, data_dict)
