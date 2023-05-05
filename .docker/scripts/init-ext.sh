e623da618b06aa5bde9a3b6635cc42e1223719ac
ThrawnCA <shell_layer-github@yahoo.com.au>
2023-05-05 13:53:48 +1000

    [QOLDEV-312] enable activity plugin during tests if present
    - This will make CKAN 2.10 behave more like 2.9 out of the box

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -46,4 +46,7 @@ installed_name=$(grep '^\s*name=' setup.py |sed "s|[^']*'\([-a-zA-Z0-9]*\)'.*|\1
 # Validate that the extension was installed correctly.
 if ! pip list | grep "$installed_name" > /dev/null; then echo "Unable to find the extension in the list"; exit 1; fi
 
+if [ -d "$SRC_DIR/ckan/ckanext/activity" ]; then
+    sed -i 's|^ckan.plugins = |ckan.plugins = activity |' $CKAN_INI
+fi
 . ${APP_DIR}/scripts/deactivate
