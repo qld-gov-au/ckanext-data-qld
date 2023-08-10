71175cdd91186d57749ff2057de3ae17f1ae1eeb
ThrawnCA <shell_layer-github@yahoo.com.au>
2023-08-10 11:22:46 +1000

    [QOLDEV-312] update test CKAN 2.10 version

--- bin/build.sh
+++ bin/build.sh
@@ -20,7 +20,7 @@ CKAN_GIT_VERSION=$CKAN_VERSION
 CKAN_GIT_ORG=qld-gov-au
 
 if [ "$CKAN_VERSION" = "2.10" ]; then
-    CKAN_GIT_VERSION=ckan-2.10.0-qgov.1
+    CKAN_GIT_VERSION=ckan-2.10.1-qgov.1
     PYTHON_VERSION=py3
     PYTHON="${PYTHON}3"
 else
