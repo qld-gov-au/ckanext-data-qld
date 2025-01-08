cd278277da3a64ee9c47613a3630ef7cf6f2a27c
ThrawnCA <shell_layer-github@yahoo.com.au>
2025-01-08 10:31:56 +1000

    [QOLDEV-1014] update test CKAN version

--- bin/build.sh
+++ bin/build.sh
@@ -24,8 +24,8 @@ if [ "$CKAN_VERSION" = "2.11" ]; then
     CKAN_GIT_VERSION=ckan-2.11.1
 elif [ "$CKAN_VERSION" = "2.10" ]; then
     CKAN_GIT_VERSION=ckan-2.10.5-qgov.4
-else
-    CKAN_GIT_VERSION=ckan-2.9.9-qgov.3
+elif [ "$CKAN_VERSION" = "2.9" ]; then
+    CKAN_GIT_VERSION=ckan-2.9.9-qgov.4
 fi
 
 sed "s|{CKAN_VERSION}|$CKAN_VERSION|g" .docker/Dockerfile-template.ckan \
