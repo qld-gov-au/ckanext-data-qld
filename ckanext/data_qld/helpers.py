1d468a939c6e1c811e3a12d91c3c264bd18a364b
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2025-02-26 12:54:04 +1000

    [QOLSVC-9308] replace problematic custom label function
    - Inject our custom display names into the 'package_search' function results,
    instead of adding a custom label function everywhere

--- ckanext/data_qld/helpers.py
+++ ckanext/data_qld/helpers.py
@@ -4,8 +4,6 @@ import datetime
 import re
 from six import text_type
 
-import jinja2
-
 from ckan import model
 from ckan.lib import uploader
 from ckantoolkit import _, g, h, abort, asbool, check_ckan_version, \
@@ -429,49 +427,3 @@ class RequestHelper():
         This uses 'request.GET' for Pylons and 'request.args' for Flask.
         """
         return self.get_query_params().get(field_name, default)
-
-
-# based on facet_list.html functionality
-# {% set label = label_function(item) if label_function else item.display_name %}
-def custom_label_function(facet_field, facet_item):
-    if facet_field == 'dataset_type':
-        if facet_item['name'] == 'dataset':
-            return 'data.qld.gov.au'
-        elif facet_item['name'] == 'geoscience':
-            return 'geoscience.data.qld.gov.au'
-
-    return jinja2.filters.do_truncate(None, s=facet_item['display_name'], length=22, leeway=5)
-
-
-# based on
-# @core_helper
-# def list_dict_filter(list_, search_field, output_field, value):
-def custom_label_function_list_dict_filter(list_, facet_field, search_field, output_field, value):
-    ''' Takes a list of dicts and returns the value of a given key if the
-    item has a matching value for a supplied key
-
-    :param list_: the list to search through for matching items
-    :type list_: list of dicts
-
-    :param facet_field: the field key which was used to find nice title
-    :type string
-
-    :param search_field: the key to use to find matching items
-    :type search_field: string
-
-    :param output_field: the key to use to output the value
-    :type output_field: string
-
-    :param value: the value to search for
-    '''
-
-    if facet_field == 'dataset_type':
-        if value == 'dataset':
-            return 'data.qld.gov.au'
-        elif value == 'geoscience':
-            return 'geoscience.data.qld.gov.au'
-
-    for item in list_:
-        if item.get(search_field) == value:
-            return item.get(output_field, value)
-    return value
