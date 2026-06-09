5edc54c3415d80d3845546d817e6133055be6148
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2026-06-09 11:00:31 +1000

    allow either text or binary headers

--- ckanext/data_qld/reporting/helpers/export_helpers.py
+++ ckanext/data_qld/reporting/helpers/export_helpers.py
@@ -158,8 +158,8 @@ def output_report_csv(csv_header_row, row_order, dict_csv_rows, report_type):
         fh = open(filepath)
 
         return fh.read(), {
-            b'Content-Type': b'text/csv; charset=utf-8',
-            b'Content-Disposition': six.ensure_binary("attachment;filename=%s" % filename)
+            'Content-Type': 'text/csv; charset=utf-8',
+            'Content-Disposition': "attachment;filename=%s" % filename
         }
     except Exception:
         log.exception('Error creating %s report CSV export file: %s',
